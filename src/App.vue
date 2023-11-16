277e98e68171d0a4694d9bb254871b689986a658
Emiliyan Ognyanov <emiliyan.ognyanov@sap.com>
2023-11-16 16:15:08 +0200

    -refactor- code clean up

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-
-  <HelloWorld msg="Hello World!" />
+  My app
 </template>
-
-<style scoped>
-
-</style>
