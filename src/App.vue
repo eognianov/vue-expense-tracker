f909930072867e3300087a7f9f30c366be195a97
Emiliyan Ognyanov <emiliyan.ognyanov@sap.com>
2023-11-16 16:57:30 +0200

    -feat- basic html structure

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
 <template>
-  My app
+  <Header/>
+  <div class="container">
+    <Balance/>
+    <IncomeExpenses/>
+    <TransactionsList/>
+    <AddTransaction/>
+  </div>
 </template>
+
+<script>
+  import Header from "./components/Header.vue";
+  import Balance from "./components/Balance.vue";
+  import IncomeExpenses from "./components/IncomeExpenses.vue";
+  import TransactionsList from "./components/TransactionsList.vue";
+  import AddTransaction from "./components/AddTransaction.vue";
+  import {defineComponent} from "vue";
+
+  export default defineComponent({
+    components: {Header, Balance, IncomeExpenses, TransactionsList, AddTransaction}
+  });
+
+</script>
