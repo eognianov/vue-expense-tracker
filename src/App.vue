4dace4944520e6059e3ce6f3f77a1af45a2086fd
Emiliyan Ognyanov <emiliyan.ognyanov@sap.com>
2023-11-28 09:55:03 +0200

    -feat- add transactions list and total

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <Header/>
   <div class="container">
-    <Balance/>
+    <Balance :total="total"/>
     <IncomeExpenses/>
-    <TransactionsList/>
+    <TransactionsList :transactions="transactions"/>
     <AddTransaction/>
   </div>
 </template>
 
-<script>
-  import Header from "./components/Header.vue";
-  import Balance from "./components/Balance.vue";
-  import IncomeExpenses from "./components/IncomeExpenses.vue";
-  import TransactionsList from "./components/TransactionsList.vue";
-  import AddTransaction from "./components/AddTransaction.vue";
-  import {defineComponent} from "vue";
+<script setup>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionsList from "./components/TransactionsList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+import {ref, computed} from "vue";
 
-  export default defineComponent({
-    components: {Header, Balance, IncomeExpenses, TransactionsList, AddTransaction}
-  });
 
+const transactions = ref([
+  {id: 1, text: 'Flower', amount: -19.99},
+  {id: 2, text: 'Games', amount: -59.99},
+]);
+
+const total = computed(()=> {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount
+  }, 0)
+});
 </script>
--- src/components/TransactionsList.vue
+++ src/components/TransactionsList.vue
@@ -2,22 +2,19 @@
   <h3>History</h3>
   <ul id="list" class="list">
     <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
-      {{transaction.text}} <span>{{transaction.amount}}</span>
+      {{ transaction.text }} <span>{{ transaction.amount }}</span>
       <button class="delete-btn">X</button>
     </li>
   </ul>
 </template>
 
-
-<script>
-  export default {
-    data() {
-      return {
-        transactions: [
-          {id:1, text: 'Flower', amount: -19.99},
-          {id:2, text: 'Games', amount: -59.99},
-        ]
+<script setup>
+const props = defineProps(
+    {
+      transactions: {
+        type: Array,
+        required: true
       }
     }
-  }
+)
 </script>
